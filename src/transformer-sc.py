5dfc4f1fe70ea9f640eb810ee54284f7c1764ef0
Simon Lee <siaulee@ucsc.edu>
2021-10-21 10:52:29 -0700

    module made

---
+++ src/transformer-sc.py
@@ -0,0 +1,7 @@
+'''
+Predicting Modalities in a single cell using the transformer: a deep learning architecture.
+'''
+__author__ = 'Vedu Maella: vedu.mallela@gmail.com, Simon Lee: siaulee@ucsc.edu'
+
+
+
