2469942064f3c96bcfd9ed0fa36ae24421cd56db
Simon Lee <siaulee@ucsc.edu>
2021-10-21 23:31:37 -0700

    misspell

--- src/transformer-sc.py
+++ src/transformer-sc.py
@@ -1,7 +1,7 @@
 '''
 Predicting Modalities in a single cell using the transformer: a deep learning architecture.
 '''
-__author__ = 'Vedu Maella: vedu.mallela@gmail.com, Simon Lee: siaulee@ucsc.edu'
+__author__ = 'Vedu Mallela: vedu.mallela@gmail.com, Simon Lee: siaulee@ucsc.edu'
 
 
 
