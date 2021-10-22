0d0bc01d1da77557e6e7d8d690fc8efbcf0c7f1c
Simon Lee <siaulee@ucsc.edu>
2021-10-22 11:53:19 -0700

    import files, and BertGenertation model

--- src/transformer-sc.py
+++ src/transformer-sc.py
@@ -3,5 +3,20 @@ Predicting Modalities in a single cell using the transformer: a deep learning ar
 '''
 __author__ = 'Vedu Mallela: vedu.mallela@gmail.com, Simon Lee: siaulee@ucsc.edu'
 
+import scanpy as sc
+import anndata as ad
+import matplotlib.pyplot as plt
+
+import numpy as np
+import pandas as pd
+import logging
+
+from transformers import BertGenerationTokenizer, BertGenerationEncoder, BertGenerationDecoder, BertGenerationConfig
+import torch
+
+from sklearn.decomposition import TruncatedSVD
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
 
 
